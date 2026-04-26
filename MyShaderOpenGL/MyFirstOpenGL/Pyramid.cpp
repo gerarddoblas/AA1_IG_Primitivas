3033cf2712c1acf6d82cd0c40fd3a5cb61ea847f
eJwtzMFqhDAQgOF3mXMOjdqouZWFUsr2UCg99BLGcbRZo0kTbSrLvnth8frD918BiTilDz/xAhoIH2vkRsmmVEpJqnhou+KhxlIyVVRXDRVKMoKAEP2v7Tke9HqczHqsxm9vTvnlK9d0Ppd9K1/X55999n0+je3wVJSf8vLWvl9AwJ2YdQ8MGjrGyBEEJPL3gP1sFx22zlkyE+8iMvbax1FEDl5siaPmGa0T2cdpcD6DAP4LNnIyuIJeNudut39NKU/3 <gerard.doblas@enti.cat>
2026-04-26 02:00:44 +0200

    Pyramid.h and .cpp

---
+++ MyShaderOpenGL/MyFirstOpenGL/Pyramid.cpp
@@ -0,0 +1,26 @@
+#include "Pyramid.h"
+
+Pyramid::Pyramid()
+{
+	Init();
+}
+
+void Pyramid::Init()
+{
+
+}
+
+void Pyramid::Update(float dt)
+{
+
+}
+
+void Pyramid::Render(const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix)
+{
+
+}
+
+void Pyramid::Input(GLFWwindow* window)
+{
+
+}
---
+++ MyShaderOpenGL/MyFirstOpenGL/Pyramid.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Primitive.h"
+
+class Pyramid : public Primitive
+{
+public:
+
+	Pyramid();
+
+	void Init();
+	void Update(float dt) override;
+	void Render(const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix) override;
+	void Input(GLFWwindow* window) override;
+private:
+
+	GLuint VAO, VBO;
+	GLuint shaderProgram;
+	ShaderProgram shader;
+
+	glm::vec3 position = glm::vec3(0.f);
+	glm::vec3 rotation = glm::vec3(0.f);
+	glm::vec3 scale = glm::vec3(1.f);
+
+	float rotationZ = 0.0f;
+};
